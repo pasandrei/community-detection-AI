56590ddd40761cffd46c6f41af0423702396662c
AlexandraL3071 <47603400+AlexandraL3071@users.noreply.github.com>
2020-05-03 18:21:34 +0300

    Implement Graph with an adjacency list (#1)
    * Implement Graph with an adjacency list
    
    * Add CMakeLists
    
    * Make changes according to suggestions

---
+++ Graph.h
@@ -0,0 +1,21 @@
+#ifndef COMMUNITY_DETECTION_AI_GRAPH_H
+#define COMMUNITY_DETECTION_AI_GRAPH_H
+
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+class Graph {
+private:
+    std::vector<std::vector<int>> adjacency_list_;
+
+public:
+    Graph(const std::string& file_name, const std::string& graph_type);
+
+    const std::vector<std::vector<int>> &get_adjacency_list() const;
+
+    void print_graph() const;
+};
+
+#endif
