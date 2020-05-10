de2c0927766e884873839c74cc7fd4908346981e
Andrei-Sorin Popovici <5727251+pasandrei@users.noreply.github.com>
2020-05-10 18:27:02 +0300

    Implementation of K-Means algorithm for graphs (#7)
    * Added default constructor for Graph
    
    * Overloaded the operator [] for Graph
    
    Now, it will return the neighbors of the desired node
    
    * Refactored Graph
    
    * Implemented K-Means algorithm for graph
    
    * Variable's name fixes

---
+++ KMeans.h
@@ -0,0 +1,39 @@
+#ifndef COMMUNITY_DETECTION_AI_KMEANS_H
+#define COMMUNITY_DETECTION_AI_KMEANS_H
+
+#include <queue>
+#include "Graph.h"
+
+class KMeans {
+    /* This class implements the K-Means algorithm. Explanation can be found:
+     * https://www.coursera.org/learn/machine-learning/lecture/93VPG/k-means-algorithm
+     * In this class I implemented this algorithm for a graph and swapped the
+     * Squared Euclidean Distance with the shortest distance between two nodes
+     */
+
+private:
+    Graph graph_;
+    int no_clusters_;
+    std::vector<std::vector<int>> clusters_;
+    std::vector<int> nodes_to_community_id_;
+
+    std::vector<int> compute_random_centroids(std::vector<int> &centroids);
+
+    std::vector<std::vector<int>> compute_clusters(const std::vector<int> &centroids);
+
+    std::vector<int> breadth_first_search(const std::vector<int> &start_nodes);
+
+    std::vector<int> get_centroids();
+
+    int get_cluster_centroid(const std::vector<int> &cluster);
+
+    int breadth_first_search_distance(int start_node);
+
+public:
+    KMeans(Graph &graph, int no_clusters);
+
+    void step();
+};
+
+
+#endif //COMMUNITY_DETECTION_AI_KMEANS_H
